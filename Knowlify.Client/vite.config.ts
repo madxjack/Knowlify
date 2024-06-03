2448bc08431f8a0626cf539078859fcb802347de
Pedro Fernández <pedrofb22@gmail.com>
2024-06-03 10:45:07 +0200

    vite config update for production

--- Knowlify.Client/vite.config.ts
+++ Knowlify.Client/vite.config.ts
@@ -1,23 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import { env } from 'process'
 import fs from 'fs'
 import child_process from 'child_process'
 
-const baseFolder =
-  env.APPDATA !== undefined && env.APPDATA !== ''
-    ? `${env.APPDATA}/ASP.NET/https`
-    : `${env.HOME}/.aspnet/https`
+// Determinar la carpeta base dependiendo del entorno de usuario
+const baseFolder = process.env.APPDATA || `${process.env.HOME}/.aspnet/https`
 
 const certificateName = 'Knowlify.Client'
 const certFilePath = path.join(baseFolder, `${certificateName}.pem`)
 const keyFilePath = path.join(baseFolder, `${certificateName}.key`)
 
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-  if (
-    0 !==
-    child_process.spawnSync(
+// Función para generar certificados en desarrollo
+function ensureCertificates() {
+  if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+    const result = child_process.spawnSync(
       'dotnet',
       [
         'dev-certs',
@@ -28,14 +25,15 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
         'Pem',
         '--no-password',
       ],
-      { stdio: 'inherit' }
-    ).status
-  ) {
-    throw new Error('Could not create certificate.')
+      { stdio: 'inherit' },
+    )
+
+    if (result.status !== 0) {
+      throw new Error('Could not create certificate.')
+    }
   }
 }
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -46,9 +44,16 @@ export default defineConfig({
   },
   server: {
     port: 5173,
-    https: {
-      key: fs.readFileSync(keyFilePath),
-      cert: fs.readFileSync(certFilePath),
-    },
+    https:
+      process.env.NODE_ENV === 'development'
+        ? {
+            key: fs.readFileSync(keyFilePath),
+            cert: fs.readFileSync(certFilePath),
+          }
+        : undefined,
   },
+  // Llamar a ensureCertificates solo en desarrollo
+  ...(process.env.NODE_ENV === 'development' && {
+    onConfigResolved: ensureCertificates,
+  }),
 })
